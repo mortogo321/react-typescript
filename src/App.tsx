4d612ca43305e5bacddee54c7f471246a67c80dd
Mor <mortogo321@gmail.com>
2023-09-03 17:04:57 +0700

    add support sass

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ function App() {
   return (
     <>
       <h1 className="p-4 text-center text-3xl font-bold underline">Vite - React Typescript</h1>
+      <button className="border rounded-lg px-3 py-1">
+        <span className="material-symbols-outlined">home</span>
+        Home
+      </button>
     </>
   );
 }
